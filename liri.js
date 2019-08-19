2bee3200c5b9b156b5641922c57b6ab4f0961390
Summer Stevenson <50718811+SummerDarcel@users.noreply.github.com>
2019-08-19 12:39:49 -0700

    added variables

--- liri.js
+++ liri.js
@@ -1,9 +1,33 @@
 require("dotenv").config();
+var fs = require('fs');
+var keys = require("./keys.js");
+var moment = require('moment');
 
 //Add the code required to import the keys.js file and store it in a variable
 
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotifyKeys = keys.spotify;
+var spotifyID = spotifyKeys.id;
+var spotifySecret = spotifyKeys.secret;
+var spotify = new Spotify({
+    id: spotifyID,
+    secret: spotifySecret
+});
+
+var axios = require("axios");
+var bandsintown = keys.bandsInTown;
+var bandsInTownID = bandsintown.id;
+
+var OMDB = require("omdb-client");
+var omdbKeys = keys.omdb;
+var omdbAPIKEY = omdbKeys.key;
+
+
+
+
+
+
+
 
 //concert-this
 //spotify-this-song
